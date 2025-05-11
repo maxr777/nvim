db09e59f6c4949f55f2c5cc1fb301e5eba110e15
max <maxr.0307@gmail.com>
2025-05-11 19:30:29 +0200

    Add claude's keybinds, TODO: check them out later

--- init.lua
+++ init.lua
@@ -20,10 +20,30 @@ vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', { noremap = true, silent
 -- Column at 80 characters
 vim.opt.colorcolumn = "80"
 
+-- below are claude's keybinds, TODO: check if they make sense
 vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
 vim.keymap.set('n', 'K', vim.lsp.buf.hover)
 vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename)
 vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action)
+
+vim.opt.relativenumber = true  -- Relative line numbers
+vim.opt.scrolloff = 8          -- Keep cursor 8 lines from screen edge while scrolling
+vim.opt.signcolumn = "yes"     -- Always show sign column for LSP diagnostics
+vim.opt.updatetime = 250       -- Faster updates for gitsigns and diagnostics
+vim.opt.timeoutlen = 300       -- Shorter timeout for which-key
+
+vim.keymap.set('n', '<C-h>', '<C-w>h', { noremap = true })
+vim.keymap.set('n', '<C-j>', '<C-w>j', { noremap = true })
+vim.keymap.set('n', '<C-k>', '<C-w>k', { noremap = true })
+vim.keymap.set('n', '<C-l>', '<C-w>l', { noremap = true })
+
+vim.keymap.set('n', '<leader>bn', ':bnext<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>bp', ':bprevious<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>bd', ':bdelete<CR>', { noremap = true, silent = true })
+
+vim.opt.ignorecase = true      -- Case-insensitive search
+vim.opt.smartcase = true       -- Unless uppercase is present
+vim.keymap.set('n', '<Esc>', ':nohlsearch<CR>', { noremap = true, silent = true })
 
 -- Load plugin manager
 require("config.lazy")
