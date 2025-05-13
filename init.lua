67af2be4d1af0015a996eb2155db6a4c5fc6f581
max <maxr.0307@gmail.com>
2025-05-13 21:10:36 +0200

    Add a formatting plugin

--- init.lua
+++ init.lua
@@ -1,49 +1,12 @@
--- Leader key
+vim.opt.relativenumber = true -- Relative line numbers
+vim.opt.scrolloff = 8 -- Keep cursor 8 lines from screen edge while scrolling
+vim.opt.signcolumn = "yes" -- Always show sign column for LSP diagnostics
+vim.opt.updatetime = 250 -- Faster updates for gitsigns and diagnostics
+vim.opt.timeoutlen = 300 -- Shorter timeout for which-key
+vim.opt.colorcolumn = "80"
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
-
--- Use the system clipboard
 vim.opt.clipboard = "unnamedplus"
-
--- Indent settings
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
-
--- Indent selection
-vim.api.nvim_set_keymap('v', '<Tab>', '>gv', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', '<S-Tab>', '<gv', { noremap = true, silent = true })
-
--- nvim-tree
-vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
-
--- Column at 80 characters
-vim.opt.colorcolumn = "80"
-
-vim.keymap.set("n", "<leader>ld", function()
-  vim.diagnostic.open_float(nil, { border = "rounded", max_width = 80 })
-end, { desc = "Show diagnostics in floating window" })
-
--- format-on-save
-vim.api.nvim_create_autocmd(
-  "BufWritePost",
-  {
-    pattern = {"*.java", "*.c", "*.cpp", "*.h", "*.hpp", "*.py", "*.js", "*.go"}, 
-    callback = function()
-      if vim.fn.expand('%:e') == 'java' then
-        vim.cmd("silent !google-java-format -i %")
-      elseif vim.fn.expand('%:e') == 'cpp' or vim.fn.expand('%:e') == 'c' or vim.fn.expand('%:e') == 'h' then
-        vim.cmd("silent !clang-format -i %")
-      elseif vim.fn.expand('%:e') == 'py' then
-        vim.cmd("silent !black %")
-      elseif vim.fn.expand('%:e') == 'js' then
-        vim.cmd("silent !prettier --write %")
-      elseif vim.fn.expand('%:e') == 'go' then
-        vim.cmd("silent !gofmt -w %")
-      end
-    end,
-  }
-)
 
 -- Resize splits
 vim.keymap.set("n", "<C-Up>", ":resize +1<CR>", { noremap = true, silent = true })
@@ -51,44 +14,73 @@ vim.keymap.set("n", "<C-Down>", ":resize -1<CR>", { noremap = true, silent = tru
 vim.keymap.set("n", "<C-Left>", ":vertical resize -1<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-Right>", ":vertical resize +1<CR>", { noremap = true, silent = true })
 
--- ToggleTerm
-vim.keymap.set('n', '<C-\\>', '<cmd>ToggleTerm direction=float<cr>', { noremap = true, silent = true })
-vim.keymap.set('n', '<F7>', '<cmd>ToggleTerm direction=horizontal<cr>', { noremap = true, silent = true })
-vim.keymap.set('n', '<F8>', '<cmd>ToggleTerm direction=vertical<cr>', { noremap = true, silent = true })
-vim.keymap.set('t', '<C-\\>', '<cmd>ToggleTerm<cr>', { noremap = true, silent = true })
-vim.keymap.set('t', '<F7>', '<cmd>ToggleTerm direction=horizontal<cr>', { noremap = true, silent = true })
-vim.keymap.set('t', '<F8>', '<cmd>ToggleTerm direction=vertical<cr>', { noremap = true, silent = true })
+-- Indent settings
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+-- Indent selection
+vim.keymap.set("v", "<Tab>", ">gv", { noremap = true, silent = true })
+vim.keymap.set("v", "<S-Tab>", "<gv", { noremap = true, silent = true })
 
 -- Map <Esc> in terminal mode to switch to normal mode
-vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]], { noremap = true })
-
--- LSP
-vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
-vim.keymap.set('n', 'K', vim.lsp.buf.hover)
-vim.keymap.set('n', '<leader>lr', vim.lsp.buf.rename)
-vim.keymap.set('n', '<leader>la', vim.lsp.buf.code_action)
-
-vim.opt.relativenumber = true  -- Relative line numbers
-vim.opt.scrolloff = 8          -- Keep cursor 8 lines from screen edge while scrolling
-vim.opt.signcolumn = "yes"     -- Always show sign column for LSP diagnostics
-vim.opt.updatetime = 250       -- Faster updates for gitsigns and diagnostics
-vim.opt.timeoutlen = 300       -- Shorter timeout for which-key
+vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { noremap = true })
 
 -- Split switching
-vim.keymap.set('n', '<C-h>', '<C-w>h', { noremap = true })
-vim.keymap.set('n', '<C-j>', '<C-w>j', { noremap = true })
-vim.keymap.set('n', '<C-k>', '<C-w>k', { noremap = true })
-vim.keymap.set('n', '<C-l>', '<C-w>l', { noremap = true })
+vim.keymap.set("n", "<C-h>", "<C-w>h", { noremap = true })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { noremap = true })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { noremap = true })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { noremap = true })
 
 -- Buffer navigation
-vim.keymap.set('n', '<leader>bn', ':bnext<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>bp', ':bprevious<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>bd', ':bdelete<CR>', { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>bn", ":bnext<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>bp", ":bprevious<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>bd", ":bdelete<CR>", { noremap = true, silent = true })
 
 -- Search settings
-vim.opt.ignorecase = true      -- Case-insensitive search
-vim.opt.smartcase = true       -- Unless uppercase is present
-vim.keymap.set('n', '<Esc>', ':nohlsearch<CR>', { noremap = true, silent = true }) -- Esc clears the search highlight without canceling the search
+vim.opt.ignorecase = true -- Case-insensitive search
+vim.opt.smartcase = true -- Unless uppercase is present
+vim.keymap.set("n", "<Esc>", ":nohlsearch<CR>", { noremap = true, silent = true }) -- Esc clears the search highlight without canceling the search
 
--- Load plugin manager
+-- Format on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*",
+	callback = function(args)
+		require("conform").format({ bufnr = args.buf })
+	end,
+})
+
+-- nvim-tree
+vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
+
+-- ToggleTerm
+vim.keymap.set("n", "<C-\\>", "<cmd>ToggleTerm direction=float<cr>", { noremap = true, silent = true })
+vim.keymap.set("n", "<F7>", "<cmd>ToggleTerm direction=horizontal<cr>", { noremap = true, silent = true })
+vim.keymap.set("n", "<F8>", "<cmd>ToggleTerm direction=vertical<cr>", { noremap = true, silent = true })
+vim.keymap.set("t", "<C-\\>", "<cmd>ToggleTerm<cr>", { noremap = true, silent = true })
+vim.keymap.set("t", "<F7>", "<cmd>ToggleTerm direction=horizontal<cr>", { noremap = true, silent = true })
+vim.keymap.set("t", "<F8>", "<cmd>ToggleTerm direction=vertical<cr>", { noremap = true, silent = true })
+
+-- LSP
+vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+vim.keymap.set("n", "K", vim.lsp.buf.hover)
+vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename)
+vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action)
+
+vim.keymap.set("n", "<leader>ld", function()
+	vim.diagnostic.open_float(nil, { border = "rounded", max_width = 80 })
+end, { desc = "Show diagnostics in floating window" })
+
+vim.diagnostic.config({
+	virtual_text = false,
+	underline = true,
+	signs = true,
+	severity_sort = true,
+	float = {
+		source = "always",
+		header = "",
+		prefix = "",
+	},
+})
+
 require("config.lazy")
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,106 +1,128 @@
 return {
-  {
-    'numToStr/Comment.nvim',
-    opts = {},
-    config = function()
-      require('Comment').setup()
-    end
-  },
-  {
-    'nvim-treesitter/nvim-treesitter',
-    build = ":TSUpdate",
-    config = function() 
-      local configs = require("nvim-treesitter.configs")
-      configs.setup({
-        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
-        sync_install = false,
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-    end
-  },
-  {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-      require('lualine').setup({
-        options = {
-          theme = "auto",
-        },
-      })
-    end
-  },
-  {
-    'lewis6991/gitsigns.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-      require('gitsigns').setup()
-    end
-  },
-  {
-    'karb94/neoscroll.nvim',
-    opts = {},
-    config = function()
-      require('neoscroll').setup()
-    end
-  },
-  {
-    'nvim-tree/nvim-tree.lua',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-      require("nvim-tree").setup()
-    end
-  },
-  {
-    'neovim/nvim-lspconfig',
-    config = function()
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.clangd.setup({})
-      lspconfig.ts_ls.setup({})
-      lspconfig.html.setup({})
-    end
-  },
-  {
-    'williamboman/mason.nvim',
-    config = function()
-      require("mason").setup()
-    end
-  },
-  {
-    'williamboman/mason-lspconfig.nvim',
-    dependencies = { 'williamboman/mason.nvim' },
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "clangd", "ts_ls", "html" },
-        automatic_installation = true,
-      })
-    end
-  },
-  {
-    'windwp/nvim-autopairs',
-    config = function()
-      require('nvim-autopairs').setup()
-    end,
-  },
-  {
-    'folke/which-key.nvim',
-    config = function()
-      require('which-key').setup()
-    end,
-  },
-  {
-    'sainnhe/gruvbox-material',
-    config = function()
-      vim.cmd('colorscheme gruvbox-material')
-      vim.cmd('set background=dark')
-      vim.g.gruvbox_material_background = 'soft'
-      vim.g.gruvbox_material_foreground = 'soft'
-      vim.g.gruvbox_material_palette = 'material'
-    end
-  },
-  {
-    'akinsho/toggleterm.nvim', version = "*",
-    opts = {}
-  },
+	{
+		"numToStr/Comment.nvim",
+		opts = {},
+		config = function()
+			require("Comment").setup()
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local configs = require("nvim-treesitter.configs")
+			configs.setup({
+				ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+				sync_install = false,
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("lualine").setup({
+				options = {
+					theme = "auto",
+				},
+			})
+		end,
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("gitsigns").setup()
+		end,
+	},
+	{
+		"karb94/neoscroll.nvim",
+		opts = {},
+		config = function()
+			require("neoscroll").setup()
+		end,
+	},
+	{
+		"nvim-tree/nvim-tree.lua",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("nvim-tree").setup()
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({})
+			lspconfig.clangd.setup({})
+			lspconfig.ts_ls.setup({})
+			lspconfig.html.setup({})
+		end,
+	},
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = { "williamboman/mason.nvim" },
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "clangd", "ts_ls", "html" },
+				automatic_installation = true,
+			})
+		end,
+	},
+	{
+		"windwp/nvim-autopairs",
+		config = function()
+			require("nvim-autopairs").setup()
+		end,
+	},
+	{
+		"folke/which-key.nvim",
+		config = function()
+			require("which-key").setup()
+		end,
+	},
+	{
+		"sainnhe/gruvbox-material",
+		config = function()
+			vim.cmd("colorscheme gruvbox-material")
+			vim.cmd("set background=dark")
+			vim.g.gruvbox_material_background = "soft"
+			vim.g.gruvbox_material_foreground = "soft"
+			vim.g.gruvbox_material_palette = "material"
+		end,
+	},
+	{
+		"akinsho/toggleterm.nvim",
+		version = "*",
+		opts = {},
+	},
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		---@module "ibl"
+		---@type ibl.config
+		opts = {},
+	},
+	{
+		"stevearc/conform.nvim",
+		opts = {},
+		config = function()
+			require("conform").setup({
+				formatters_by_ft = {
+					c = { "clang-format" },
+					cpp = { "clang-format" },
+					lua = { "stylua" },
+					python = { "black" },
+				},
+			})
+		end,
+	},
 }
