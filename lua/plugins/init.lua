0bb6eb2e2e94012d26aaf4be082eabd9a66e478a
max <maxr.0307@gmail.com>
2025-05-14 18:45:17 +0200

    Change vim-surround to its nvim equivalent, yss now works

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -126,7 +126,12 @@ return {
 		end,
 	},
 	{
-		"tpope/vim-surround",
+		"kylechui/nvim-surround",
+		version = "^3.0.0",
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({})
+		end,
 	},
 	{
 		"unblevable/quick-scope",
